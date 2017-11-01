b094ea7fcb757d3fa86a8dbb9b9577c8ab410312
Brendan Davis <brendanmd@gmail.com>
2017-11-01 14:02:07 -0700

    working but still need tests.

---
+++ priority_queue.py
@@ -0,0 +1,69 @@
+"""Create a Queue consisting of Nodes."""
+
+
+class Node(object):
+    """A Node which contains a value."""
+
+    def __init__(self, data, previous, priority=99):
+        """Create a new Node."""
+        self.data = data
+        self.previous = previous
+        self.next_node = None
+
+
+class Priority_Q(object):
+    """A Line of Nodes."""
+
+    def __init__(self):
+        """Create an empty queue."""
+        self.head = None
+        self.tail = None
+        self._counter = 0
+
+    def insert(self, val):
+        """Add a node to the tail of the queue."""
+        new_tail = Node(val, self.tail)
+        if self.tail is None:
+            self.head = new_tail
+            self.tail = new_tail
+        else:
+            self.tail.next_node = new_tail
+            self.tail = new_tail
+        self._counter += 1
+
+
+    def pop(self):
+        """Remove a node from the head of the queue."""
+        if not self.head:
+            raise IndexError("There is nothing to dequeue.")
+        output = self.head
+        curr = self.head
+        while curr:
+            if curr.priority < output.priority:
+                output = curr
+            curr = curr.next_node
+        if output.next_node:
+            output.next_node.previous = output.previous
+            output.previous.next_node = output.next_node
+            self.counter -= 1
+        elif self.counter is 1:
+            self.head = None
+            self.tail = None
+            self._counter -= 1
+        else:
+            self.tail = self.tail.previous
+            self.tail.next = None
+            self.counter -= 1
+        return output.data
+
+    def peek(self):
+        """Access the value of the head of the queue."""
+        return self.head.data
+
+    def size(self):
+        """Return the size of the queue."""
+        return self._counter
+
+    def __len__(self):
+        """Work with len() function to find length of linked list."""
+        return self._counter
---
+++ test_priorityq.py
@@ -0,0 +1,134 @@
+"""Test for the queue."""
+
+
+import pytest
+
+
+def test_node_has_attributes():
+    """Test for Node."""
+    from priority_queue import Node
+    n = Node(1, None)
+    assert hasattr(n, 'data')
+    assert hasattr(n, 'data')
+
+
+def test_empty_queue_does_not_have_head():
+    """."""
+    from priority_queue import Priority_Q
+    q = Priority_Q()
+    assert q.head is None
+
+
+def test_empty_queue_does_not_have_tail():
+    """."""
+    from priority_queue import Priority_Q
+    q = Priority_Q()
+    assert q.tail is None
+
+
+def test_insert_creates_head():
+    """."""
+    from priority_queue import Priority_Q
+    q = Priority_Q()
+    q.insert('x')
+    assert q.head.data is 'x'
+
+
+def test_insert_creates_tail():
+    """."""
+    from priority_queue import Priority_Q
+    q = Priority_Q()
+    q.insert('x')
+    assert q.tail.data is 'x'
+
+
+def test_two_items_in_line_correct_head():
+    """If two items queue up the head is the first."""
+    from priority_queue import Priority_Q
+    q = Priority_Q()
+    q.insert('x')
+    q.insert('y')
+    assert q.head.data is 'x'
+
+
+def test_two_items_in_line_correct_tail():
+    """If two items queue up the tail is the second one."""
+    from priority_queue import Priority_Q
+    q = Priority_Q()
+    q.insert('x')
+    q.insert('y')
+    assert q.tail.data is 'y'
+
+
+def test_popping_leaves_correct_head():
+    """If two items queue up and one dequeues the second is the new head."""
+    from priority_queue import Priority_Q
+    q = Priority_Q()
+    q.insert('x')
+    q.insert('y')
+    q.insert('z')
+    q.pop()
+    assert q.head.data == 'y'
+
+
+def test_len_works_properly():
+    """If two items queue up and one dequeues the second is the new head."""
+    from priority_queue import Priority_Q
+    q = Priority_Q()
+    q.insert('x')
+    q.insert('y')
+    assert len(q) == 2
+
+
+def peek_works():
+    """Peekaboo."""
+    from priority_queue import Priority_Q
+    q = Priority_Q()
+    q.insert('x')
+    q.insert('y')
+    assert q.peek == 'x'
+
+
+def test_size_works_properly():
+    """If two items queue up and one dequeues the second is the new head."""
+    from priority_queue import Priority_Q
+    q = Priority_Q()
+    q.insert('x')
+    q.insert('y')
+    assert q.size() == 2
+
+
+def test_next_is_correct():
+    """If two items queue up and one dequeues the second is the new head."""
+    from priority_queue import Priority_Q
+    q = Priority_Q()
+    q.insert('x')
+    q.insert('y')
+    assert q.size() == 2
+
+def priority_remove_works():
+    """If two items queue up and one dequeues the second is the new head."""
+    from priority_queue import Priority_Q
+    q = Priority_Q()
+    q.insert('x', None, 0)
+    q.insert('y', None, 1)
+    assert q.pop() == 'x'
+
+def priority_remove_works():
+    """If two items queue up and one dequeues the second is the new head."""
+    from priority_queue import Priority_Q
+    q = Priority_Q()
+    q.insert('z', None, 0)    
+    q.insert('x', None, 0)
+    q.insert('y', None, 1)
+    assert q.pop() == 'z'
+
+def priority_remove_works():
+    """If two items queue up and one dequeues the second is the new head."""
+    from priority_queue import Priority_Q
+    q = Priority_Q()
+    q.insert('z', None, 2)    
+    q.insert('x', None, 2)
+    q.insert('y', None, 0)
+    assert q.pop() == 'y'
+
