6903a06238bb436b9393fcd9da766e3774ee2e25
Rob Bronson <robert.j.bronson@gmail.com>
2017-11-01 15:19:00 -0700

    fixed errors from grading

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,46 +1,89 @@
-"""."""
+"""Singly linked list creation."""
 
 
 class Node(object):
-    """."""
+    """Creates a node object."""
 
-    def __init__(self, val, next=None):
-         """Create a new instantiation of a node."""
-        self.val = val
-        self.next = next
-
-
-    def get_data(self):
-        return self.val
-
-    def get_next(self):
-        return self.next
-
-    def set_next(self, new_next):
-        self.next = new_next
+    def __init__(self, data, next_node):
+        """Constructor for the Node object."""
+        self.data = data
+        self.next = next_node
 
 
 class LinkedList(object):
-    """."""
+    """Class for containing object called LinkedList."""
 
-    def __init__(self, head=None):
-        """."""
-        self.head = head
-
+    def __init__(self, iterable=()):
+        """Constructor for the Linked List object."""
+        self.head = None
+        self._counter = 0
+        if hasattr(iterable, '__iter__') or isinstance(iterable, str):
+            for item in iterable:
+                self.push(item)
 
     def push(self, val):
-        """."""
-        new Node(val)
-        node.next = head
+        """Add a new value to the head of the linked list."""
+        new_head = Node(val, self.head)
+        self.head = new_head
+        self._counter += 1
 
+    def pop(self):
+        """Remove & return the value of the head of the Linked List."""
+        if not self.head:
+            raise IndexError("The list is empty, so there's nothing to pop.")
+        output = self.head.data
+        self.head = self.head.next
+        self._counter -= 1
+        return output
 
-    def remove(self, val)
+    def size(self):
+        """Return the size of our list."""
+        return self._counter
 
+    def __len__(self):
+        """Work with len() function to find length of linked list."""
+        return self._counter
 
-def search(self, val)
+    def search(self, val):
+        """Search for a given node value and returns it."""
+        curr = self.head
+        if not curr:
+            return
+        while curr:
+            if curr.data == val:
+                return curr
+            curr = curr.next
+        return
 
-def display()
+    def remove(self, val):
+        """Search for a given value and remove it from the linked list."""
+        curr = self.head
+        while curr:
+            if curr.next == val:
+                curr.next = curr.next.next
+                self._counter -= 1
+                return
+            curr = curr.next
 
-def pop()
+    def display(self):
+        """Will return a string representing the list.
 
-# next=None
+        as if it were a Python tuple literal: "(12, ‘sam’, 37, ‘tango’)"
+        """
+        if self.head is None:
+            return
+        curr = self.head
+        output = "("
+        while curr:
+            if isinstance(curr.data, (int, float)):
+                output += "{}, ".format(str(curr.data))
+            else:
+                output += "'{}', ".format(str(curr.data))
+            curr = curr.next
+        output = output[:-2]
+        output += ")"
+        return output
+
+    def __repr__(self):
+        """Print the list to the screen using built in print()."""
+        return self.display()
--- src/test_linked_list.py
+++ src/test_linked_list.py
@@ -6,7 +6,7 @@ import pytest
 
 def test_node_has_attributes():
     """Test for Node."""
-    from linked_list_code_base import Node
+    from linked_list import Node
     n = Node(1, None)
     assert hasattr(n, 'data')
     assert hasattr(n, 'data')
@@ -14,14 +14,14 @@ def test_node_has_attributes():
 
 def test_linked_list_has_head():
     """."""
-    from linked_list_code_base import LinkedList
+    from linked_list import LinkedList
     l = LinkedList()
     assert l.head is None
 
 
 def test_linked_list_push_adds_new_item():
     """."""
-    from linked_list_code_base import LinkedList
+    from linked_list import LinkedList
     l = LinkedList()
     l.push('val')
     assert l.head.data == 'val'
@@ -29,7 +29,7 @@ def test_linked_list_push_adds_new_item():
 
 def test_linked_list_push_two_new_item():
     """."""
-    from linked_list_code_base import LinkedList
+    from linked_list import LinkedList
     l = LinkedList()
     l.push('val')
     l.push('val2')
@@ -38,7 +38,7 @@ def test_linked_list_push_two_new_item():
 
 def test_linked_list_push_moves_old_head_to_new_heads_next():
     """."""
-    from linked_list_code_base import LinkedList
+    from linked_list import LinkedList
     l = LinkedList()
     l.push('val')
     l.push('val2')
@@ -47,7 +47,7 @@ def test_linked_list_push_moves_old_head_to_new_heads_next():
 
 def test_linked_list_pop_removes_head_and_returns_value():
     """."""
-    from linked_list_code_base import LinkedList
+    from linked_list import LinkedList
     l = LinkedList()
     l.push('potato')
     l.pop()
@@ -56,7 +56,7 @@ def test_linked_list_pop_removes_head_and_returns_value():
 
 def test_linked_list_returns_head_value():
     """."""
-    from linked_list_code_base import LinkedList
+    from linked_list import LinkedList
     l = LinkedList()
     l.push('potato')
     assert l.pop() == ('potato')
@@ -64,7 +64,7 @@ def test_linked_list_returns_head_value():
 
 def test_linked_list_pop_shifts_head_properly():
     """."""
-    from linked_list_code_base import LinkedList
+    from linked_list import LinkedList
     l = LinkedList()
     l.push('potato')
     l.push('cabbage')
@@ -74,7 +74,7 @@ def test_linked_list_pop_shifts_head_properly():
 
 def test_linked_list_pop_empty_raises_exception():
     """."""
-    from linked_list_code_base import LinkedList
+    from linked_list import LinkedList
     l = LinkedList()
     with pytest.raises(IndexError):
         l.pop()
@@ -82,7 +82,7 @@ def test_linked_list_pop_empty_raises_exception():
 
 def test_linked_list_size_returns_list_length():
     """."""
-    from linked_list_code_base import LinkedList
+    from linked_list import LinkedList
     l = LinkedList()
     assert l.size() == 0
 
@@ -90,7 +90,7 @@ def test_linked_list_size_returns_list_length():
 @pytest.mark.parametrize('n', range(100))
 def test_linked_list_size_returns_list_length2(n):
     """."""
-    from linked_list_code_base import LinkedList
+    from linked_list import LinkedList
     l = LinkedList()
     for i in range(n):
         l.push(i)
@@ -99,7 +99,7 @@ def test_linked_list_size_returns_list_length2(n):
 
 def test_linked_list_search__empty_returns_none():
     """."""
-    from linked_list_code_base import LinkedList
+    from linked_list import LinkedList
     l = LinkedList()
     l.push(1)
     assert l.search(1).data is 1
@@ -107,7 +107,7 @@ def test_linked_list_search__empty_returns_none():
 
 def test_linked_list_search__with_one_returns_node():
     """."""
-    from linked_list_code_base import LinkedList
+    from linked_list import LinkedList
     l = LinkedList()
     l.push(1)
     assert l.search(1) == l.head
@@ -115,7 +115,7 @@ def test_linked_list_search__with_one_returns_node():
 
 def test_linked_list_search__with_one_bad_search():
     """."""
-    from linked_list_code_base import LinkedList
+    from linked_list import LinkedList
     l = LinkedList()
     l.push(1)
     assert l.search(0) is None
@@ -124,7 +124,7 @@ def test_linked_list_search__with_one_bad_search():
 # @pytest.mark.parametrize('n', range(1, 10))
 # def test_linked_list_search__with_one_bad_search2(n):
 #     """."""
-#     from linked_list_code_base import LinkedList
+#     from linked_list import LinkedList
 #     from random import randint
 #     l = LinkedList()
 #     for i in range(1, n + 1):
@@ -133,37 +133,53 @@ def test_linked_list_search__with_one_bad_search():
 #     assert l.search(search_me).data == search_me
 
 
-def test_linked_list_ca_take_iterable():
+def test_linked_list_can_take_iterable_str():
     """."""
-    from linked_list_code_base import LinkedList
+    from linked_list import LinkedList
+    a_name = "bilbo baggins"
+    l = LinkedList(a_name)
+    for item in a_name:
+        assert l.search(item).data == item
+
+
+def test_linked_list_can_take_iterable_range():
+    """."""
+    from linked_list import LinkedList
+    a_list = range(1, 20)
+    l = LinkedList(a_list)
+    for item in a_list:
+        assert l.search(item).data == item
+
+
+def test_linked_list_can_take_iterable_list():
+    """."""
+    from linked_list import LinkedList
     a_list = [4, 3, 2, 6, 4, 9, 8]
     l = LinkedList(a_list)
     for item in a_list:
-        #pass
         assert l.search(item).data == item
 
 
 def test_remove_removes_number_from_list():
     """."""
-    from linked_list_code_base import LinkedList
+    from linked_list import LinkedList
     a_list = [4, 3, 2, 6, 4, 9, 8]
     l = LinkedList(a_list)
-    l.remove(6)
+    l.remove(l.search(6))
     assert len(l) == 6
-    assert l.search(6) == None
+    assert l.search(6) is None
+
+
+def test_display_on_empty_list():
+    """."""
+    from linked_list import LinkedList
+    l = LinkedList()
+    assert l.display() is None
 
 
 def test_display_a_list():
     """."""
-    from linked_list_code_base import LinkedList
+    from linked_list import LinkedList
     a_list = [4, 3, 2, 6, 4, 9, 8]
     l = LinkedList(a_list)
     assert l.display() == "(8, 9, 4, 6, 2, 3, 4)"
-
-
-def test_print_the_list():
-    """."""
-    from linked_list_code_base import LinkedList
-    a_list = [4, 3, 2, 6, 4, 9, 8]
-    l = LinkedList(a_list)
-    assert l.print() == "(8, 9, 4, 6, 2, 3, 4)"
