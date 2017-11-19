fac22740dae6c6934f5f99b771f977bd109a0680
Rob Bronson <robert.j.bronson@gmail.com>
2017-11-19 13:22:52 -0800

    wrapped code in function

--- src/djs_algorithm.py
+++ src/djs_algorithm.py
@@ -1,35 +1,43 @@
+"""Run Djikstra's algorithm to find shortest path on a graph."""
 
 
+def djs_algorithm(start_pt, end_pt, node_list, edge_list):
+    """Implement the algorithm."""
+    nodelist = node_list  # ['s', 'a', 'b', 'c', 'd', 't']
 
-nodelist[s, a, b, c, d, t]
+    pred_list = {}
+    for node in nodelist:
+        pred_list[node] = ['', float('inf')]
+    pred_list[start_pt] = ['', 0]  # {'s': ['', 0]}
 
-pred_list = {'s':['', 0]}
-for node in nodelist:
-    pred_list[node] = ['', float('inf')]
+    edges = edge_list
+    # edges = {('s', 'a'): 2,
+    #          ('s', 'b'): 1,
+    #          ('a', 'c'): 8,
+    #          ('a', 'b'): 4,
+    #          ('b', 'a'): 2,
+    #          ('b', 's'): 4,
+    #          ('b', 'd'): 2,
+    #          ('c', 'a'): 2,
+    #          ('c', 'd'): 7,
+    #          ('c', 't'): 4,
+    #          ('d', 'b'): 1,
+    #          ('d', 'c'): 11,
+    #          ('t', 'c'): 3,
+    #          ('t', 'd'): 5
+    #          }
+    for node in nodelist:
 
-edges = {
-         (s, a) = 2,
-         (s, b) = 1,
-         (a, c) = 8,
-         (a, b) = 4,
-         (b, a) = 2,
-         (b, s) = 4,
-         (b, d) = 2,
-         (c, a) = 2,
-         (c, d) = 7
-         (c, t) = 4,
-         (d, b) = 1,
-         (d, c) = 11,
-         (t, c) = 3,
-         (t, d) = 5
-    }
+        for key in edges:
+            if key[0] == node:
+                tentative = edges[key] + pred_list[key[0]](1)
+                if tentative < pred_list[key[1]](1):
+                    pred_list[key[1]] = [key[0], tentative]
+    dist = pred_list[end_pt](1)
+    route = []
+    cur = end_pt
+    while cur:
+        route.append(pred_list[cur](0))
+        cur = pred_list[cur](0)
 
-
-for node in nodelist:
-
-    for edge in edges:
-        if edge(0) = node:
-            tentative = edges[edge] + pred_list[edge(0)](1)
-            if pred_list contains edge(1):
-                if tentative < pred_list[edge(1)](1):
-                    pred_list[edge(1)] = [edge(0), tentative]
+    return dist, route
